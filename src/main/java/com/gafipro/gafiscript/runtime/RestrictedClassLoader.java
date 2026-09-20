f9faefbb5a9f0f7bf124a12c34983ded7b6b88a0
GafiPro <gabrielriosguedes@gmail.com>
2026-09-20 12:57:47 +0100

    feat: add restricted script class loader

---
+++ src/main/java/com/gafipro/gafiscript/runtime/RestrictedClassLoader.java
@@ -0,0 +1,40 @@
+package com.gafipro.gafiscript.runtime;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+
+public final class RestrictedClassLoader extends URLClassLoader {
+    private static final List<String> BLOCKED_PREFIXES = List.of(
+            "java.io.",
+            "java.net.",
+            "java.nio.file.",
+            "java.lang.reflect.",
+            "java.lang.invoke.",
+            "sun.",
+            "jdk.internal."
+    );
+
+    public RestrictedClassLoader(URL[] urls, ClassLoader parent) {
+        super(urls, parent);
+    }
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        for (String prefix : BLOCKED_PREFIXES) {
+            if (name.startsWith(prefix)) {
+                throw new ClassNotFoundException("Blocked class: " + name);
+            }
+        }
+
+        if (name.equals("java.lang.Runtime")
+                || name.equals("java.lang.ProcessBuilder")
+                || name.equals("java.lang.System")
+                || name.equals("java.lang.ClassLoader")
+                || name.equals("jdk.internal.misc.Unsafe")) {
+            throw new ClassNotFoundException("Blocked class: " + name);
+        }
+
+        return super.loadClass(name, resolve);
+    }
+}
