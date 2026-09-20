3f3c41c8df8aaa538375e91d1973af1025c7a5e6
GafiPro <gabrielriosguedes@gmail.com>
2026-09-20 12:55:37 +0100

    feat: add GafiScript common initializer

---
+++ src/main/java/com/gafipro/gafiscript/GafiScriptMod.java
@@ -0,0 +1,34 @@
+package com.gafipro.gafiscript;
+
+import com.gafipro.gafiscript.api.Gafi;
+import com.gafipro.gafiscript.command.GafiScriptCommands;
+import com.gafipro.gafiscript.net.GafiScriptNetworking;
+import com.gafipro.gafiscript.registry.ModBlockEntities;
+import com.gafipro.gafiscript.registry.ModBlocks;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class GafiScriptMod implements ModInitializer {
+    public static final String MOD_ID = "gafiscript";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static Identifier id(String path) {
+        return Identifier.of(MOD_ID, path);
+    }
+
+    @Override
+    public void onInitialize() {
+        ModBlocks.register();
+        ModBlockEntities.register();
+        GafiScriptNetworking.registerCommon();
+        GafiScriptCommands.register();
+
+        ServerLifecycleEvents.SERVER_STARTED.register(Gafi::attachServer);
+        ServerLifecycleEvents.SERVER_STOPPING.register(Gafi::detachServer);
+
+        LOGGER.info("GafiScript initialized.");
+    }
+}
