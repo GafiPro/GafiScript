b62f754982a780ad8ade07244ea2bec292150856
GafiPro <gabrielriosguedes@gmail.com>
2026-09-20 13:43:05 +0100

    feat: add scoreboard and title APIs

---
+++ src/main/java/com/gafipro/gafiscript/api/GafiTitles.java
@@ -0,0 +1,30 @@
+package com.gafipro.gafiscript.api;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
+import net.minecraft.network.packet.s2c.play.SubtitleS2CPacket;
+
+public final class GafiTitles {
+    private GafiTitles() {}
+
+    public static void title(
+            GafiPlayer player,
+            String title
+    ) {
+        ServerPlayerEntity raw = player.raw();
+        raw.networkHandler.send(
+                new TitleS2CPacket(Text.literal(title))
+        );
+    }
+
+    public static void subtitle(
+            GafiPlayer player,
+            String subtitle
+    ) {
+        ServerPlayerEntity raw = player.raw();
+        raw.networkHandler.send(
+                new SubtitleS2CPacket(Text.literal(subtitle))
+        );
+    }
+}
