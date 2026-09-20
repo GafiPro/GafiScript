bca98df02e5f6759de4a85d96f254188cb9e4187
GafiPro <gabrielriosguedes@gmail.com>
2026-09-20 13:28:28 +0100

    fix: update core APIs for Minecraft 1.21.11

--- src/main/java/com/gafipro/gafiscript/registry/ModBlockEntities.java
+++ src/main/java/com/gafipro/gafiscript/registry/ModBlockEntities.java
@@ -2,13 +2,14 @@ package com.gafipro.gafiscript.registry;
 
 import com.gafipro.gafiscript.GafiScriptMod;
 import com.gafipro.gafiscript.block.GafiScriptBlockEntity;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public final class ModBlockEntities {
     public static final BlockEntityType<GafiScriptBlockEntity> GAFI_SCRIPT =
-            BlockEntityType.Builder.create(
+            FabricBlockEntityTypeBuilder.create(
                     GafiScriptBlockEntity::new,
                     ModBlocks.GAFI_SCRIPT_BLOCK
             ).build();
