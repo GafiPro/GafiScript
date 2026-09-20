dfc36c3546990d9f497e6e300fad02f8f22fba95
GafiPro <gabrielriosguedes@gmail.com>
2026-09-20 20:19:21 +0100

    feat: forward mouse wheel scrolling in project editor

--- src/main/java/com/gafipro/gafiscript/client/GafiProjectScreen.java
+++ src/main/java/com/gafipro/gafiscript/client/GafiProjectScreen.java
@@ -363,6 +363,29 @@ public final class GafiProjectScreen extends Screen {
     }
 
     @Override
+    public boolean mouseScrolled(
+            double mouseX,
+            double mouseY,
+            double horizontalAmount,
+            double verticalAmount
+    ) {
+        if (editor.mouseScrolled(
+                mouseX,
+                mouseY,
+                verticalAmount
+        )) {
+            return true;
+        }
+
+        return super.mouseScrolled(
+                mouseX,
+                mouseY,
+                horizontalAmount,
+                verticalAmount
+        );
+    }
+
+    @Override
     public void render(
             DrawContext context,
             int mouseX,
